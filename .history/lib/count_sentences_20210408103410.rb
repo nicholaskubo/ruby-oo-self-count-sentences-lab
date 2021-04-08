567bcdb85ba9901ade37734316479f8eb1cc98b2
Nicholas Kubo <nickskubo@gmail.com>
2021-04-08 10:51:26 -0700

    commit tree 132cfd234a2aa0dc181972b1e9b9ad453fc58a42

--- .history/lib/count_sentences_20210408103410.rb
+++ .history/lib/count_sentences_20210408103410.rb
@@ -7,14 +7,14 @@ class String
   end
 
   def question?
-    self.end_with?("?")
+
   end
 
   def exclamation?
-    self.end_with?("!")
+
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if{|x| x.size < 2}.count
+
   end
 end
--- .history/lib/count_sentences_20210408103609.rb
+++ .history/lib/count_sentences_20210408103609.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if{|x| x.size < 2}.count
+
   end
 end
